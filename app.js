a00114471868c9addfa3873870e62fff2eea6a96
brcvda <s.fioraso.brc.vda@gmail.com>
2026-09-10 08:58:24 +0200

    Enhance playTrack function with loading and error handling
    Updated playTrack function to load audio before playing and added error handling.

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const player = document.getElementById("audioPlayer");
 const title = document.getElementById("track-title");
 
 function playTrack(url, nome) {
-    player.src = url;
     title.textContent = nome;
-    player.play();
+    player.src = url;
+    player.load();
+
+    player.play().catch(error => {
+        console.error("Errore riproduzione:", error);
+    });
 }
