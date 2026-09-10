79a39854260f5a7ebc99a9a14766262e8eb3cad4
brcvda <s.fioraso.brc.vda@gmail.com>
2026-09-10 08:39:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const player = document.getElementById("audioPlayer");
+const title = document.getElementById("track-title");
 
+function playTrack(url, nome){
+
+    player.src = url;
+
+    title.textContent = nome;
+
+    player.play();
+}
